a18e4737f973c49abc33ca0dddae7a9023a80442
Ian Leifer <ianleifer93@gmail.com>
2020-11-17 18:41:51 -0500

    Functions is more flexible now

--- R/functions.R
+++ R/functions.R
@@ -1,23 +1,38 @@
-#' Find balanced coloring using Kamei algorithm
-#'
-#' This function finds a balanced coloring
-#'
-#' @param raw_edges list of edges
-#' @return Data frame with 2 columns: Name (for the node name) and Color (for )
-#' @export
-get.balanced.coloring.Kamei <- function(raw_edges = NA, file = NA, sep = "\t", header = F, directed = F, weighted = F, look.for.no.input.nodes = T) {
+get.raw.edges <- function(raw_edges = NA, file = NA, sep = "\t", header = F) {
+  if(!is.na(file) & !is.na(raw_edges)) {
+    stop("Both raw_edges and file are specified, specify only one")
+  }
+
   if(!is.na(file)) {
     if(is.na(raw_edges)) {
       raw_edges <- read.table(file, sep = " ", header = F, stringsAsFactors = F, quote = "")
-      weighted = as.logical(ncol(raw_edges) - 2)
     } else {
-      stop("No file or edgelist specified, please see usage manual")
+      stop("No file or edgelist specified, please see manual for usage")
     }
   } else {
     raw_edges[] <- apply(raw_edges, 2, as.character)
   }
 
-  Rcpp::sourceCpp("kamei_coloring.cpp")
+  return(raw_edges)
+}
+
+#' Find balanced coloring using Kamei algorithm
+#'
+#' This function finds a balanced coloring
+#'
+#' @param raw_edges list of edges
+#' @param file list of edges
+#' @param sep list of edges
+#' @param header list of edges
+#' @param directed list of edges
+#' @param look.for.no.input.nodes list of edges
+#' @return Data frame with 2 columns: Name (for the node name) and Color (for )
+#' @export
+get.balanced.coloring.Kamei <- function(raw_edges = NA, file = NA, sep = "\t", header = F, directed = F, look.for.no.input.nodes = T) {
+  raw_edges = get.raw.edges(raw_edges = raw_edges, file = file, sep = sep, header = header)
+  weighted = as.logical(ncol(raw_edges) - 2)
+
+  # Rcpp::sourceCpp("/home/ian/Dropbox (City College)/Research/fibrationSymmetries/R/kamei_coloring.cpp")
   graph = igraph::graph_from_edgelist(as.matrix(raw_edges[, 1:2]), directed = directed)
 
   integer.nodes = igraph::as_data_frame(graph, what = "vertices")
